0bc65c68b60a89c791541a8df8c9177ecea05d9a
eyaBENABDALLAH <78912803+eya-benabdallah@users.noreply.github.com>
2022-11-13 23:00:46 +0100

    Update Operateur.java

--- src/main/java/tn/esprit/rh/achat/entities/Operateur.java
+++ src/main/java/tn/esprit/rh/achat/entities/Operateur.java
@@ -1,16 +1,17 @@
 package tn.esprit.rh.achat.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,18 +22,110 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Operateur implements Serializable{
-	private static final long serialVersionUID = 1L;
+public class Produit implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idOperateur;
-	private String nom;
-	private String prenom;
-	
-	private String password;
-	@OneToMany
+	private Long idProduit;
+	private String codeProduit;
+	private String libelleProduit;
+	private float prix;
+	@Temporal(TemporalType.DATE)
+	private Date dateCreation;
+	@Temporal(TemporalType.DATE)
+	private Date dateDerniereModification;
+	@ManyToOne
 	@JsonIgnore
-	private Set<Facture> factures;
+	private Stock stock;
+	@OneToMany(mappedBy = "produit")
+	@JsonIgnore
+	private Set<DetailFacture> detailFacture;
+	@ManyToOne
+	@JsonIgnore
+	private CategorieProduit categorieProduit;
+	public Long getIdProduit() {
+		return idProduit;
+	}
+	public void setIdProduit(Long idProduit) {
+		this.idProduit = idProduit;
+	}
+	public String getCodeProduit() {
+		return codeProduit;
+	}
+	public void setCodeProduit(String codeProduit) {
+		this.codeProduit = codeProduit;
+	}
+	public String getLibelleProduit() {
+		return libelleProduit;
+	}
+	public void setLibelleProduit(String libelleProduit) {
+		this.libelleProduit = libelleProduit;
+	}
+	public float getPrix() {
+		return prix;
+	}
+	public void setPrix(float prix) {
+		this.prix = prix;
+	}
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+	public Date getDateDerniereModification() {
+		return dateDerniereModification;
+	}
+	public void setDateDerniereModification(Date dateDerniereModification) {
+		this.dateDerniereModification = dateDerniereModification;
+	}
+	public Stock getStock() {
+		return stock;
+	}
+	public void setStock(Stock stock) {
+		this.stock = stock;
+	}
+	public Set<DetailFacture> getDetailFacture() {
+		return detailFacture;
+	}
+	public void setDetailFacture(Set<DetailFacture> detailFacture) {
+		this.detailFacture = detailFacture;
+	}
+	public CategorieProduit getCategorieProduit() {
+		return categorieProduit;
+	}
+	public void setCategorieProduit(CategorieProduit categorieProduit) {
+		this.categorieProduit = categorieProduit;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public Produit(Long idProduit, String codeProduit, String libelleProduit, float prix, Date dateCreation,
+			Date dateDerniereModification, Stock stock, Set<DetailFacture> detailFacture,
+			CategorieProduit categorieProduit) {
+		super();
+		this.idProduit = idProduit;
+		this.codeProduit = codeProduit;
+		this.libelleProduit = libelleProduit;
+		this.prix = prix;
+		this.dateCreation = dateCreation;
+		this.dateDerniereModification = dateDerniereModification;
+		this.stock = stock;
+		this.detailFacture = detailFacture;
+		this.categorieProduit = categorieProduit;
+	}
+	public Produit(long l, Object object, Object object2, Object object3, Object object4, Object object5, Object object6, Object object7, Object object8, Object object9) {
+		super();
+	}
+	public Produit() {
+		// TODO Auto-generated constructor stub
+	}
+
+
 	
+
 }
