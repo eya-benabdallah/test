0aabe17928ecb7158ae3590164b02be376b255da
eyaBENABDALLAH <78912803+eya-benabdallah@users.noreply.github.com>
2022-11-14 13:03:46 +0100

    Update Produit.java

--- src/main/java/tn/esprit/rh/achat/entities/Produit.java
+++ src/main/java/tn/esprit/rh/achat/entities/Produit.java
@@ -20,8 +20,6 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Produit implements Serializable {
 
 	/**
@@ -47,7 +45,83 @@ public class Produit implements Serializable {
 	@ManyToOne
 	@JsonIgnore
 	private CategorieProduit categorieProduit;
-	
+	public Long getIdProduit() {
+		return idProduit;
+	}
+	public void setIdProduit(Long idProduit) {
+		this.idProduit = idProduit;
+	}
+	public String getCodeProduit() {
+		return codeProduit;
+	}
+	public void setCodeProduit(String codeProduit) {
+		this.codeProduit = codeProduit;
+	}
+	public String getLibelleProduit() {
+		return libelleProduit;
+	}
+	public void setLibelleProduit(String libelleProduit) {
+		this.libelleProduit = libelleProduit;
+	}
+	public float getPrix() {
+		return prix;
+	}
+	public void setPrix(float prix) {
+		this.prix = prix;
+	}
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+	public Date getDateDerniereModification() {
+		return dateDerniereModification;
+	}
+	public void setDateDerniereModification(Date dateDerniereModification) {
+		this.dateDerniereModification = dateDerniereModification;
+	}
+	public Stock getStock() {
+		return stock;
+	}
+	public void setStock(Stock stock) {
+		this.stock = stock;
+	}
+	public Set<DetailFacture> getDetailFacture() {
+		return detailFacture;
+	}
+	public void setDetailFacture(Set<DetailFacture> detailFacture) {
+		this.detailFacture = detailFacture;
+	}
+	public CategorieProduit getCategorieProduit() {
+		return categorieProduit;
+	}
+	public void setCategorieProduit(CategorieProduit categorieProduit) {
+		this.categorieProduit = categorieProduit;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public Produit(Long idProduit, String codeProduit, String libelleProduit, float prix, Date dateCreation,
+			Date dateDerniereModification, Stock stock, Set<DetailFacture> detailFacture,
+			CategorieProduit categorieProduit) {
+		super();
+		this.idProduit = idProduit;
+		this.codeProduit = codeProduit;
+		this.libelleProduit = libelleProduit;
+		this.prix = prix;
+		this.dateCreation = dateCreation;
+		this.dateDerniereModification = dateDerniereModification;
+		this.stock = stock;
+		this.detailFacture = detailFacture;
+		this.categorieProduit = categorieProduit;
+	}
+	public Produit(long l, Object object, Object object2, Object object3, Object object4, Object object5, Object object6, Object object7, Object object8, Object object9) {
+		super();
+	}
+	public Produit() {
+		// TODO Auto-generated constructor stub
+	}
 
 
 	
